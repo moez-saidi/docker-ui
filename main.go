bf52fcef3ee3ca18815774d52d8bfe088ad0a11e
Moez <saidi.moez..2014@gmail.com>
2024-11-03 22:13:15 +0100

    Add Gin routes to list containers and images

--- main.go
+++ main.go
@@ -2,43 +2,39 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/docker/docker/api/types"
 	containertypes "github.com/docker/docker/api/types/container"
 
 	imagetypes "github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 )
 
-func ListImages(ctx context.Context, cli *client.Client) {
+const Port = ":10000"
+
+func ListImages(ctx context.Context, cli *client.Client) ([]imagetypes.Summary, error) {
 	images, err := cli.ImageList(ctx, imagetypes.ListOptions{})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	for _, image := range images {
-		imageBytes, _ := json.Marshal(image)
-		fmt.Println(string(imageBytes))
-	}
+	return images, nil
 }
 
-func ListContainers(ctx context.Context, cli *client.Client) {
+func ListContainers(ctx context.Context, cli *client.Client) ([]types.Container, error) {
 	containers, err := cli.ContainerList(ctx, containertypes.ListOptions{})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	for _, container := range containers {
-		containerBytes, _ := json.Marshal(container)
-		fmt.Println(string(containerBytes))
-	}
+	return containers, nil
 }
 
 func main() {
-	gin.Default()
+	router := gin.Default()
 
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -47,7 +43,25 @@ func main() {
 	}
 	defer cli.Close()
 
-	ListImages(ctx, cli)
-	ListContainers(ctx, cli)
+	router.GET("/containers", func(c *gin.Context) {
+		containers, err := ListContainers(ctx, cli)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, containers)
+	})
+
+	router.GET("/images", func(c *gin.Context) {
+		images, err := ListImages(ctx, cli)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, images)
+	})
+	router.Run(Port)
 
 }
