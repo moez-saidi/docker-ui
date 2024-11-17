9ebb79d7ea613acc5e92b1b4a5c9f97a4a4c6017
Moez <saidi.moez.2014@gmail.com>
2024-11-17 18:21:56 +0100

    FIx

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"docker-ui/internal/app/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,5 +12,8 @@ const Port string = ":10000"
 func main() {
 	router := gin.Default()
 	routes.RegisterRoutes(router)
-	router.Run(Port)
+	if err := router.Run(Port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
 }
